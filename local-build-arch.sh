856befae1c4b391fbb57c94220aacbe5c2a6df51
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-06-16 20:24:36 +0200

    #8 Add support for linux/arm/v6

---
+++ local-build-arch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TODAY=$(date '+%Y-%m-%d')
+
+echo "TODAY=${TODAY}"
+
+docker buildx build . \
+    --progress=plain \
+    --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6 \
+    --tag giof71/airconnect:latest \
+    --tag giof71/airconnect:stable \
+    --tag giof71/airconnect:debian \
+    --tag giof71/airconnect:debian-${TODAY} \
+    --push
